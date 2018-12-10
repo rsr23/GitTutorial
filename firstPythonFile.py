6d286b0e648320d8ad4669aced33f559821b7fe3
RsR-b <45771888+RsR-b@users.noreply.github.com>
2018-12-11 02:03:02 +0530

    Update firstPythonFile.py

--- firstPythonFile.py
+++ firstPythonFile.py
@@ -4,8 +4,13 @@ def triangle(height):
   for i in range(1,height):
     print('*' * i)
   return 0
+def line(length):
+  print('-' * length)
+  return 0
+
 
 if __name__ == '__main__':
   print(f'In the main function execution\nAuthor : Ravisankar')
   print(power(10, 3))
   triangle(5)
+  line(20)
